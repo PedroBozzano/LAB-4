f6e8ea44006edb7197bd7410f3b8a6c23ffdf85a
BozzanoO <dev.bozzano@gmail.com>
2024-08-13 21:42:06 -0300

    My first commit

---
+++ springbootlab4/src/test/java/lab4/spring/springbootlab4/Springbootlab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package lab4.spring.springbootlab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootlab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
